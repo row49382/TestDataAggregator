9cc9b69120030a067cea835a837753fc9d375fb0
Robert Wroblewski <r-wroblewski@bethel.edu>
2021-02-19 15:16:44 -0600

    updating unit tests with factory

---
+++ TestInformationAggregator.Tests/Tests/TestInformationBuilderFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestInformationAggregator.Tests.Tests
+{
+	[TestFixture]
+	public class TestInformationBuilderFactoryTests
+	{
+		public void Test()
+		{
+
+		}
+	}
+}
